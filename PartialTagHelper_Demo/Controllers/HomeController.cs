30a3f445805a20664631f26827a4efd7aa344b39
Farhan Ahmed <farhan1086@gmail.com>
2020-01-05 00:46:12 +0530

    inital commit

---
+++ PartialTagHelper_Demo/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PartialTagHelper_Demo.Data;
+using PartialTagHelper_Demo.Models;
+
+namespace PartialTagHelper_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext  dbContext;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            dbContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            var employee = dbContext.Employees.ToList();
+            return View(employee);
+        }
+
+        public PartialViewResult Employees()
+        {
+            var employee = dbContext.Employees.ToList();
+            return PartialView("_Employee", employee);
+        }
+
+    }
+}
---
+++ PartialTagHelper_Demo/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PartialTagHelper_Demo.Models;
+
+namespace PartialTagHelper_Demo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
